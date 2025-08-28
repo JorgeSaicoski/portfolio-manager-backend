5108e0808fc009abec1b6d8fc18ea1a6f7bf9f57
Jorge Sarkis <jorgesaicoski@gmail.com>
2025-08-28 13:37:35 -0300

    create and update improved

--- internal/models/project.go
+++ internal/models/project.go
@@ -1,20 +1,17 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Project struct {
 	gorm.Model
-	Title       string    `json:"title"`
-	Images      []string  `json:"images" gorm:"type:text[]"` // ["/uploads/projects/1/img1.jpg", "/uploads/projects/1/img2.jpg"]
-	MainImage   string    `json:"main_image"`                // "/uploads/projects/1/main.jpg"
-	Description string    `json:"description" gorm:"type:text"`
-	Skills      []string  `json:"skills" gorm:"type:text[]"`
-	Client      string    `json:"client"`
-	Link        string    `json:"link"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	Title       string   `json:"title"`
+	Images      []string `json:"images" gorm:"type:text[]"`
+	MainImage   string   `json:"main_image"`
+	Description string   `json:"description" gorm:"type:text"`
+	Skills      []string `json:"skills" gorm:"type:text[]"`
+	Client      string   `json:"client"`
+	Link        string   `json:"link"`
+	OwnerID     string   `json:"ownerId,omitempty"`
 }
