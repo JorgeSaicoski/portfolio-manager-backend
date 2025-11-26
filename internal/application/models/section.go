023609fbb4889b03d8fd1438cc34ad374424c060
JorgeSaicoski <jorgesaicoski@gmail.com>
2025-11-26 13:37:03 -0300

    Add comment for `Type` field in `section.go` explaining possible values

--- internal/application/models/section.go
+++ internal/application/models/section.go
@@ -6,7 +6,7 @@ type Section struct {
 	gorm.Model
 	Title       string           `json:"title"`
 	Description *string          `json:"description,omitempty"`
-	Type        string           `json:"type"`
+	Type        string           `json:"type"` // Optional, could be NavBar, HomePageSection ....
 	Position    uint             `json:"position" gorm:"default:0"`
 	OwnerID     string           `json:"ownerId,omitempty"`
 	PortfolioID uint             `json:"portfolio_id"`
